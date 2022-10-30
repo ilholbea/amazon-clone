38297ef9121939d9404b772bdbd83a9ac616f364
Ionut-Laurentiu Holbea <ilholbea@gmail.com>
2022-10-30 17:52:41 +0100

    Header done/ Home in progress

---
+++ src/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="home__container">
+        <img
+          src="https://m.media-amazon.com/images/I/61I-51kLmfL._SX3000_.jpg"
+          alt="banner"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyBtb1Bts7RC0SZQagj5wK-z2l1QB-At_mU",
+    authDomain: "clone-54239.firebaseapp.com",
+    projectId: "clone-54239",
+    storageBucket: "clone-54239.appspot.com",
+    messagingSenderId: "869798989603",
+    appId: "1:869798989603:web:d4160c60c5d8d9d3748696",
+    measurementId: "G-M7Y5G08Q5P"
+};
